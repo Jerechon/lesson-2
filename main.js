6d93c09f0221b6181f10aaea413cc491a699bf64
Ierehon <vlad.ierehon@gmail.com>
2023-09-20 15:17:57 +0300

    update

--- main.js
+++ main.js
@@ -2,45 +2,62 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+const createUser = async ({ name, email }) => {
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  console.log(user);
+  return user;
+};
+
+const createProduct = async ({ title, size, description, compound, price }) => {
+  const product = await prisma.product.create({
+    data: {
+      title,
+      size,
+      description,
+      compound,
+      price,
+    },
+  });
+  console.log(product);
+  return product;
+};
+
+const createPurchase = async ({ userId, productId }) => {
+  const purchase = await prisma.userProducts.create({
+    data: {
+      userId,
+      productId,
+    },
+  });
+  console.log(purchase);
+  return purchase;
+};
+
 async function main() {
-  //   const user = await prisma.user.create({
-  //     data: {
-  //       name: "Vasya",
-  //       email: "vasya@prisma.io",
-  //     },
-  //   });
-
-  //   const post = await prisma.post.create({
-  //     data: {
-  //       title: "Im born",
-  //       content: "toomorow",
-  //       authorId: user.id,
-  //     },
-  //   });
-  //   console.log(user);
-  //   console.log(post);
-
+  // const user = await createUser({ name: "Vasya", email: "vasya@88date.co" });
+  // const product = await createProduct({
+  //   title: "Hoodie",
+  //   size: "28",
+  //   description: "Just Hoodie",
+  //   compound: "100% Silk",
+  //   price: "100$",
+  // });
+  // await createPurchase({ userId: user.id, productId: product.id });
   const users = await prisma.user.findMany({
-    where: {
-      name: "Alice",
-      posts: {
-        some: {
-          title: "Im born",
+    include: {
+      shoppingList: {
+        include: {
+          product: true,
         },
       },
     },
-    include: {
-      posts: true,
-    },
   });
-
-  console.log(users);
-
-  const user = await prisma.user.findUnique({
-    where: { email: "vasya@prisma.io" },
-  });
-
-  console.log(user);
+  console.log(users[2].shoppingList);
 }
 
 main()
